193d25bb72c7cc44c017a1bb5e774863ffb16cb0
Thiffany <thiffanygabrielle48@gmail.com>
2022-11-08 15:57:41 +0000

    create function CreateGame in main js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function createGame(player1, hour, player2) {
-    return <li >
-        <
-        img src = "http://" <
-        /li>
+    return `
+    <li>
+        <img src="/assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+        <strong>${hour}</strong>
+        <img src="/assets/icon-${player2}.svg" alt="Bandeira do ${player2}" />
+    </li>
+    `
 }
